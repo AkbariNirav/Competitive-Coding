986ced18bfec15add5434b7d93b4ee2fdd55c084
Nirav Akbari <AkbariNirav@users.noreply.github.com>
2017-09-12 23:12:12 +0530

    Maximum Draws Hacker Rank Mathematics Fundamentals

---
+++ Hacker Rank/Mathematics/Fundamentals/Maximum Draws.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+ 
+int main() {
+    int t;
+    scanf("%d",&t);
+    int i=0;
+    for(i=0;i<t;i++)
+        {
+        int n;
+        scanf("%d",&n);
+        printf("%d\n",n+1);
+    }
+    return 0;
+}
